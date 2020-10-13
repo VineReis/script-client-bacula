68d9d5c7feefccb3d4aa72537a6d3c9cb6e60e5c
VineReis <viniciusreis1001@gmail.com>
2020-10-13 10:42:59 -0300

    Atualizado o script para a ultima versao do Bacula

--- instala-bacula-client.sh
+++ instala-bacula-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # CRIAÇÃO: VINICIUS REIS
 # DATA DE CRIAÇÃO: 12/08/2020
+# DATA DE MODIFICAÇÃO: 13/10/2020
 # SCRIPT PARA INSTALAÇÃO DE CLIENT-BACULA
 
 # CRIANDO PASTA PARA BAIXAR O BACULA
@@ -14,11 +15,11 @@ apt-get install wget -y
 
 # BAIXANDO O BACULA NA PASTA CRIADA ANTERIORMENTE
 
-wget 'https://sourceforge.net/projects/bacula/files/bacula/9.4.4/bacula-9.4.4.tar.gz' -O /home/suporte/bacula/bacula-9.4.4.tar.gz
+wget 'https://sourceforge.net/projects/bacula/files/bacula/9.6.5/bacula-9.6.5.tar.gz' -O /home/suporte/bacula/bacula-9.6.5.tar.gz
 
 # DESCOMPACTANDO O BACULA BAIXADO NA PASTA /USR/SRC/
 
-tar -xvzf /home/suporte/bacula/bacula-9.4.4.tar.gz -C /usr/src/
+tar -xvzf /home/suporte/bacula/bacula-9.6.5.tar.gz -C /usr/src/
 
 # INSTALANDO ALGUMAS DEPENDENCIAS
 
@@ -26,11 +27,11 @@ apt-get install -y build-essential zlib1g-dev liblzo2-dev libacl1-dev libssl-dev
 
 # E VAMOS PARA A PRIMEIRA COMPILAÇÃO
 
-cd /usr/src/bacula-9.4.4/ && ./configure --enable-client-only --enable-build-dird=no --enable-build-stored=no --bindir=/usr/bin --sbindir=/usr/sbin --with-scriptdir=/etc/bacula/scripts --with-working-dir=/var/lib/bacula --with-logdir=/var/log --enable-smartalloc
+cd /usr/src/bacula-9.6.5/ && ./configure --enable-client-only --enable-build-dird=no --enable-build-stored=no --bindir=/usr/bin --sbindir=/usr/sbin --with-scriptdir=/etc/bacula/scripts --with-working-dir=/var/lib/bacula --with-logdir=/var/log --enable-smartalloc
 
 # E VAMOS PARA A SEGUNDA COMPILAÇÃO
 
-cd /usr/src/bacula-9.4.4/ && make -j8 && make install && make install-autostart-fd
+cd /usr/src/bacula-9.6.5/ && make -j8 && make install && make install-autostart-fd
 
 # INICIANDO OS SISTEMAS E RESTARTANDO ELES
 
